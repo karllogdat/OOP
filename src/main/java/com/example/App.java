b94661efebd059ebe90673bb2bc09134e407921c
karllogdat <logdatk@gmail.com>
2024-12-29 12:28:48 +0800

    configured repo as maven project

---
+++ src/main/java/com/example/App.java
@@ -0,0 +1,102 @@
+package com.example;
+
+import java.util.Scanner;
+
+public class App {
+	public static void main(String args[]) {
+		Scanner console = new Scanner(System.in);
+
+		System.out.println("====================================================");
+		System.out.println("String Manipulation Toolkit");
+		System.out.println("====================================================");
+		System.out.println("To use the tool, enter a valid choice between 1-10.");
+		System.out.println("Then you will be prompted to enter 3 sentences, each");
+		System.out.println("sentence is separated by a newline.");
+		System.out.println("====================================================");
+
+		System.out.print("");
+		System.out.println("Enter choice between 1-10: ");
+		System.out.println("    [1] : String Reversal");
+		System.out.println("    [2] : Palindrome Checker");
+		System.out.println("    [3] : Anagram Checker");
+		System.out.println("    [4] : Word Count");
+		System.out.println("    [5] : Character Count");
+		System.out.println("    [6] : Substring Finder");
+		System.out.println("    [7] : Lower Case Converter");
+		System.out.println("    [8] : Upper Case Converter");
+		System.out.println("    [9] : Vowel Remover");
+		System.out.println("    [10]: Consonant Remover");
+
+		System.out.print("");
+		System.out.print("CHOICE   >> ");
+		int iChoice = console.nextInt();
+		console.nextLine();
+		System.out.print("");
+
+		System.out.print("ENTER STRING >> ");
+		String userSentences = console.nextLine();
+		System.out.print("");
+
+		switch (iChoice) {
+			case 1:
+				System.out.println("REVERSED >> " + StringToolkit.reverse(userSentences));
+				break;
+
+			case 2: 
+				System.out.println("IS PALINDROME? >> " + StringToolkit.isPalindrome(userSentences));
+				break;
+
+			case 3: 
+				System.out.print("ENTER STRING TO COMPARE TO >> ");
+				String compareAgainst = console.nextLine();
+				System.out.println("Comparing against \"" + compareAgainst + "\"");
+				System.out.println("IS ANAGRAM? >> " + StringToolkit.isAnagram(userSentences, compareAgainst));
+				break;
+
+			case 4: 
+				System.out.println("WORD COUNT >> " + StringToolkit.wordCount(userSentences));
+				break;
+				
+			case 5:
+				System.out.println("CHAR COUNT >> " + StringToolkit.characterCount(userSentences));
+				break;
+
+			case 6: 
+				// TODO: fix substringFinder()
+				// substringFinder() has problems, 
+				// try str = "this this this this"
+				// try substr = "this"
+				// expected output: 0, 5, 10, 15
+				// actual output  : 0, 5, 10
+				System.out.print("ENTER SUBSTRING >> ");
+				String substr = console.nextLine();
+				System.out.println("Substring \"" + substr + "\" is found at indexes: ");
+				for (int index : StringToolkit.substringFinder(userSentences, substr)) {
+					System.out.println("    [" + index + "]");
+				}
+				break;
+
+			case 7:
+				System.out.println("LOWERCASED >> " + StringToolkit.toLowerCase(userSentences));
+				break;
+
+                        case 8:
+				System.out.println("UPPERCASED >> " + StringToolkit.toUpperCase(userSentences));
+				break;
+
+			case 9:
+				System.out.println("NO VOWELS >> " + StringToolkit.removeVowels(userSentences));
+				break;
+
+			case 10:
+				System.out.println("NO CONSONANTS >> " + StringToolkit.removeConsonants(userSentences));
+				break;
+
+			default: 
+				System.out.println("Invalid choice: " + iChoice);
+				break;
+		}
+
+		console.close();
+	}
+}
---
+++ src/main/java/com/example/StringToolkit.java
@@ -0,0 +1,178 @@
+package com.example;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class StringToolkit {
+	/**
+	 * Converts a string to lowercase, trims extra whitespace, and removes
+	 * unnecessary symbols
+	 *
+	 * @param str string to convert
+	 * @return normalized string
+	 */
+	private static String clean(String str) {
+		str = str.toLowerCase();
+		str = str.trim();
+		str = str.replaceAll("[^a-zA-Z0-9]", "");
+
+		return str;
+	}
+
+	/**
+	 * Reverses a string
+	 * 
+	 * @param str string to reverse
+	 * @return reversed string
+	 */
+	public static String reverse(String str) {
+		char[] reversed = new char[str.length()];
+
+		for (int i = 0; i < str.length(); i++) {
+			reversed[str.length() - 1 - i] = str.charAt(i);
+		}
+
+		return new String(reversed);
+	}
+
+	/**
+	 * Checks whether a given string is a palindrome
+	 * 
+	 * @param str string to check
+	 */
+	public static boolean isPalindrome(String str) {
+		if (str == null || str.isEmpty()) {
+			return false;
+		}
+
+		str = clean(str);
+
+		int mid = str.length() / 2;
+		int startIndex = 0;
+		int endIndex = str.length() - 1;
+
+		while (startIndex <= mid) {
+			if (str.charAt(startIndex) != str.charAt(endIndex)) {
+				return false;
+			}
+
+			startIndex++;
+			endIndex--;
+		}
+
+		return true;
+	}
+
+	/**
+	 * Checks whether or not two strings are anagrams.
+	 * <p>
+	 * Before checking, the two strings are normalized using the `clean()` method.
+	 * Extra symbols and whitespaces are ignored and both strings are converted
+	 * to lowercase.
+	 *
+	 * @param str1 first string
+	 * @param str2 second string
+	 */
+	public static boolean isAnagram(String str1, String str2) {
+		str1 = clean(str1);
+		str2 = clean(str2);
+
+		if (str1.length() != str2.length()) {
+			return false;
+		}
+
+		char[] str1Array = str1.toCharArray();
+		char[] str2Array = str2.toCharArray();
+
+		Arrays.sort(str1Array);
+		Arrays.sort(str2Array);
+
+		if (Arrays.equals(str1Array, str2Array)) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+
+	/**
+	 * Counts all the words in the given string 
+	 *
+	 * @param str string to count all words
+	 * @return word count
+	 */
+	public static int wordCount(String str) {
+		return str.split(" ").length;
+	}
+
+	/**
+	 * Counts all characters in the given string
+	 * <p>
+	 * Whitespaces do not count as characters and as such replaced.
+	 *
+	 * @param str string to count all characters
+	 * @return character count
+	 */
+	public static int characterCount(String str) {
+		return str.replaceAll("\\s", "").length();
+	}
+
+	public static int[] substringFinder(String str, String substr) {
+		ArrayList<Integer> indexes = new ArrayList<>();
+
+		for (int i = 0; i < str.length() - substr.length() + 1; i++) {
+			if (substr.equals(str.substring(i, i+substr.length()))) {
+				indexes.add(i);
+			}
+		}
+
+		int indexes_arr[] = new int[indexes.size()];
+		for (int i = 0; i < indexes.size(); i++) {
+			indexes_arr[i] = indexes.get(i);
+		}
+
+		return indexes_arr;
+	}
+
+	/**
+	 * Converts a string to lowercase using the built-in toLowerCase() method
+	 *
+	 * @param str string to convert
+	 * @return lowercase string 
+	 */
+	public static String toLowerCase(String str) {
+		return str.toLowerCase();
+	}
+
+        /**
+	 * Converts a string to uppercase using the built-in toUpperCase() method
+	 *
+	 * @param str string to convert
+	 * @return uppercase string 
+	 */
+	public static String toUpperCase(String str) {
+		return str.toUpperCase();
+	}
+
+	/**
+	 * Remove all vowels in given string 
+	 *
+	 * @param str target strings 
+	 * @return string with removed vowels 
+	 */
+	public static String removeVowels(String str) {
+		return str.replaceAll("[aeiouAEIOU]", "");
+	}
+
+	/**
+	 * Remove all consonants in given string 
+	 *
+	 * @param str target string 
+	 * @return string with removed consonants 
+	 */
+	public static String removeConsonants(String str) {
+		return str.replaceAll(
+				"[bcdfghjklmnpqrstvwxyzBCDFGHJKLMNPQRSTVWXYZ]",
+				""
+				);
+	}
+}
